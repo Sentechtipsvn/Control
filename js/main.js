e547a99d5ebe5e3f03569475255edf8a0f668287
Hồ Ngọc Sen <70382284+Sentechtipsvn@users.noreply.github.com>
2026-08-30 22:07:20 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const SHORTCUT_NAME = "Control";
 function runShortcut(textValue) {
     const encodedText = encodeURIComponent(textValue);
     const encodedName = encodeURIComponent(SHORTCUT_NAME);
-    const callbackUrl = encodeURIComponent(window.location.href);
+    const callbackUrl = encodeURIComponent(Control://);
     
     // x-callback-url cấu trúc chuẩn của Apple Shortcuts
     const url = `shortcuts://x-callback-url/run-shortcut?name=${encodedName}&input=text&text=${encodedText}&x-success=${callbackUrl}&x-cancel=${callbackUrl}&x-error=${callbackUrl}`;
